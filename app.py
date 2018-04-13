b2f6d511cba419794cb71424945b1abcf470538f
Pedro Dias <pmds3000@hotmail.com>
2018-04-13 22:39:55 +0100

    routes

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
+
+from flask import render_template
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("homepage.html")
 
+@app.route("/task")
+def taks():
+    return render_template("task.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
