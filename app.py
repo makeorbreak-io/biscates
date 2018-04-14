9b2c675f0f6def28f3372e31e3ba14271a1e33de
BrunoBarros21 <brunocostabarros@hotmail.com>
2018-04-14 14:46:39 +0100

    Tasks handling initiated

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask import render_template
+from controllers.task_controller import *
 
 
 app = Flask(__name__)
@@ -9,13 +10,12 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from controllers.task_controller import get_all_tasks
-
-
 
 @app.route("/")
 def hello():
     tasks = get_all_tasks()
+    for task in tasks:
+        task.user_info = get_user_by_id(task.user)
     return render_template("homepage.html", tasks=tasks)
 
 
--- controllers/task_controller.py
+++ controllers/task_controller.py
@@ -1,9 +1,14 @@
 from app import db
 from models import Tasks
+from models import Users
 
 def get_all_tasks():
-    tasks = Tasks.query.order_by(Tasks.created_at).all()
+    tasks = Tasks.query.filter_by(approved=False).order_by(Tasks.created_at).all()
     return tasks
+
+def get_user_by_id(id):
+    user = Users.query.get(id)
+    return user
 
 def get_task(id):
     task = Task.query.get(id)
